6d0b2cbe9796630565b9d5f5a0c3242860771c1b
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-12-12 00:38:18 +0100

    Create responsive on porfolio

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,11 +3,14 @@ import Experiencia from "../../pages/Experiencia/Experiencia";
 import Presentacion from "../../pages/Presentacion/Presentacion";
 import Sidebar from "../Sidebar/Sidebar";
 import { motion, useScroll } from "framer-motion";
-import "./App.css";
 import Proyectos from "../../pages/Proyectos/Proyectos";
+import BurgerSidebar from "../BurgerSidebar/BurgerSidebar";
+import "./App.css";
 
 const App = (): React.ReactElement => {
   const { scrollYProgress } = useScroll();
+
+  const isMobile = window.innerWidth <= 767;
 
   return (
     <main className="main-container">
@@ -15,7 +18,8 @@ const App = (): React.ReactElement => {
         className="progress-bar"
         style={{ scaleX: scrollYProgress }}
       />
-      <Sidebar />
+      {isMobile ? <BurgerSidebar /> : <Sidebar isOpen={false} />}
+
       <div className="body-container">
         <div id="presentacion">
           <Presentacion />
