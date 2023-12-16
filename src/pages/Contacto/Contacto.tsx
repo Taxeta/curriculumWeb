26488a83219aa6379bd059078b385f37e55b56bb
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-12-16 16:20:43 +0100

    Fix bugs on initial mobile view

--- src/pages/Contacto/Contacto.tsx
+++ src/pages/Contacto/Contacto.tsx
@@ -6,59 +6,61 @@ import maps from "../../assets/maps.png";
 
 const Contacto = (): React.ReactElement => {
   return (
-    <section className="info-contact">
-      <a href={`mailto: josemanuelvera1991@gmail.com`}>
-        <button className="info-button">
+    <section className="info-container">
+      <div className="info-contact">
+        <a href={`mailto: josemanuelvera1991@gmail.com`}>
+          <button className="info-button">
+            <img
+              className="icon-container"
+              src={email}
+              alt="email icon"
+              width={48}
+              height={48}
+            />
+            josemanuelvera1991@gmail.com
+          </button>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jose-manuel-vera-guerrero"
+          target="_blank"
+        >
+          <button className="info-button">
+            <img
+              className="icon-container"
+              src={linkedin}
+              alt="linkedin icon"
+              width={48}
+              height={48}
+            />
+            LinkedIn
+          </button>
+        </a>
+        <span className="info-button">
           <img
             className="icon-container"
-            src={email}
-            alt="email icon"
+            src={phone}
+            alt="phone icon"
             width={48}
             height={48}
           />
-          josemanuelvera1991@gmail.com
-        </button>
-      </a>
-      <a
-        href="https://www.linkedin.com/in/jose-manuel-vera-guerrero"
-        target="_blank"
-      >
-        <button className="info-button">
-          <img
-            className="icon-container"
-            src={linkedin}
-            alt="linkedin icon"
-            width={48}
-            height={48}
-          />
-          LinkedIn
-        </button>
-      </a>
-      <span className="info-button">
-        <img
-          className="icon-container"
-          src={phone}
-          alt="phone icon"
-          width={48}
-          height={48}
-        />
-        +34 665 403 133
-      </span>
-      <a
-        href="https://www.google.es/maps/place/Carrer+Josep+Vicen%C3%A7+Foix,+7,+08750+Molins+de+Rei,+Barcelona/@41.4135365,2.0253101,17z/data=!3m1!4b1!4m6!3m5!1s0x12a49a87d6372975:0x11bc4057fe136ba3!8m2!3d41.4135365!4d2.0253101!16s%2Fg%2F11bw4142pt?entry=ttu"
-        target="_blank"
-      >
-        <button className="info-button">
-          <img
-            className="icon-container"
-            src={maps}
-            alt="adress icon"
-            width={48}
-            height={48}
-          />
-          c/ Josep Vicenç Foix 7, Molins de Rei, Bcn 08750
-        </button>
-      </a>
+          +34 665 403 133
+        </span>
+        <a
+          href="https://www.google.es/maps/place/Carrer+Josep+Vicen%C3%A7+Foix,+7,+08750+Molins+de+Rei,+Barcelona/@41.4135365,2.0253101,17z/data=!3m1!4b1!4m6!3m5!1s0x12a49a87d6372975:0x11bc4057fe136ba3!8m2!3d41.4135365!4d2.0253101!16s%2Fg%2F11bw4142pt?entry=ttu"
+          target="_blank"
+        >
+          <button className="info-button">
+            <img
+              className="icon-container"
+              src={maps}
+              alt="adress icon"
+              width={48}
+              height={48}
+            />
+            c/ Josep Vicenç Foix 7, Molins de Rei, Bcn 08750
+          </button>
+        </a>
+      </div>
     </section>
   );
 };
