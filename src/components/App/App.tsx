4bbc4ea664af67dd5f19786f40cbb435c7dda522
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2024-01-23 20:43:12 +0100

    Add changes on experience and change styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,60 +6,40 @@ import { motion, useScroll } from "framer-motion";
 import Proyectos from "../../pages/Proyectos/Proyectos";
 import BurgerSidebar from "../BurgerSidebar/BurgerSidebar";
 import "./App.css";
-import { useEffect, useState } from "react";
 
 const App = (): React.ReactElement => {
-  const [showBanner, setShowBanner] = useState(true);
   const { scrollYProgress } = useScroll();
 
   const isMobile = window.innerWidth <= 767;
 
-  useEffect(() => {
-    const bannerTimer = setTimeout(() => {
-      setShowBanner(false);
-    }, 3000);
-
-    return () => clearTimeout(bannerTimer);
-  }, []);
-
   return (
     <main className="main-container">
-      {showBanner ? (
-        <div className="introduction-container">
-          <span className="presentacion__introduction">
-            Bienvenidos a mi porfolio <br />
-            soy...
-          </span>
+      <motion.div
+        className="progress-bar"
+        style={{ scaleX: scrollYProgress }}
+      />
+      {isMobile ? <BurgerSidebar /> : <Sidebar isOpen={false} />}
+      <div className="body-container">
+        <div id="presentacion">
+          <Presentacion />
         </div>
-      ) : (
-        <>
-          <motion.div
-            className="progress-bar"
-            style={{ scaleX: scrollYProgress }}
-          />
-          {isMobile ? <BurgerSidebar /> : <Sidebar isOpen={false} />}
-          <div className="body-container">
-            <div id="presentacion">
-              <Presentacion />
-            </div>
-            <div id="proyectos">
-              <Proyectos />
-            </div>
-            <div id="experiencia">
-              <Experiencia />
-            </div>
-            <div id="contacto">
-              <Contacto />
-            </div>
-            <div className="footer">
-              <span>
-                Copyright © {new Date().getFullYear()}. Todos los derechos
-                reservados.
-              </span>
-            </div>
-          </div>
-        </>
-      )}
+        <div id="proyectos">
+          <Proyectos />
+        </div>
+        <div id="experiencia">
+          <Experiencia />
+        </div>
+        <div id="contacto">
+          <Contacto />
+        </div>
+        <div className="footer">
+          <span>
+            Copyright © {new Date().getFullYear()}. Todos los derechos
+            reservados.
+          </span>
+          <span className="footer__dev">Developed by Jose Vera with ♥</span>
+        </div>
+      </div>
     </main>
   );
 };
